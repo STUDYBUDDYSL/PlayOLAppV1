5f15fe0955deffd8a7092b69c118ddf63ad03495
rangana <rangana@taloslogy.com>
2020-11-04 15:28:01 +0530

    Generate grade fragment UI on folders found on sd card

--- app/src/main/java/com/taloslogy/playolapp/GradeFragment.kt
+++ app/src/main/java/com/taloslogy/playolapp/GradeFragment.kt
@@ -1,10 +1,16 @@
 package com.taloslogy.playolapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_grade.*
+import kotlinx.android.synthetic.main.nice_button1.view.*
+import org.json.JSONObject
+import java.io.File
+import kotlin.math.ceil
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +41,74 @@ class GradeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_grade, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val json = readFileText("fileNames.json")
+        val jsonObject = JSONObject(json)
+        val files = getFilesFromPath("/storage/5E71-DBAD/Courses/Grade 10")
+
+        if (files.isNotEmpty()) {
+            // Create layout params for the table rows and children
+            val lParams = TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                3f
+            )
+            lParams.setMargins(0,10,0,10)
+
+            val cParams = TableRow.LayoutParams(
+                0,
+                TableRow.LayoutParams.WRAP_CONTENT,
+                1f
+            )
+
+            // Set row count as per item count in files
+            val rowCount = ceil(files.size.div(3.0)).toInt()
+
+            for (x in 0 until rowCount) {
+                // Create a new row with layout params
+                val row = TableRow(activity)
+                row.layoutParams = lParams
+
+                // Assign children to the created row
+                var y = 0
+                while (y < 3) {
+                    val i = (x * 3) + y
+                    if (files.size > i && jsonObject.has(files[i].name)) {
+                        // Create the fancy button
+                        val nb = LayoutInflater.from(activity).inflate(R.layout.nice_button1, null)
+
+                        nb.btn.text = jsonObject.getJSONObject(files[i].name).getString("name")
+                        nb.btn.maxLines = jsonObject.getJSONObject(files[i].name).getInt("lines")
+                        nb.layoutParams = cParams
+                        row.addView(nb)
+                    }
+                    else {
+                        val extra = TextView(activity)
+                        extra.layoutParams = cParams
+                        row.addView(extra)
+                    }
+                    y++
+                }
+                // Add the completed row to table
+                table_view.addView(row, lParams)
+            }
+        }
+    }
+
+    private fun getFilesFromPath(path: String, showHiddenFiles: Boolean = false, onlyFolders: Boolean = true): List<File> {
+        val files = File(path).listFiles() ?: return emptyList()
+        return files
+            .filter { showHiddenFiles || !it.name.startsWith(".") }
+            .filter { !onlyFolders || it.isDirectory }
+            .toList()
+    }
+
+    private fun readFileText(fileName: String): String {
+        return requireActivity().assets.open(fileName).bufferedReader().use { it.readText() }
     }
 
     companion object {
