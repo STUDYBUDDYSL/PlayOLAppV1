e5b88ec037f96ce52de97446d8f5090c252950af
rangana <rangana@taloslogy.com>
2020-11-05 14:01:51 +0530

    Add video fragment skeleton classes and navigation safe args

---
+++ app/src/main/java/com/taloslogy/playolapp/fragments/VideoFragment.kt
@@ -0,0 +1,28 @@
+package com.taloslogy.playolapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.taloslogy.playolapp.R
+import kotlinx.android.synthetic.main.fragment_video.*
+
+class VideoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val position = arguments?.let { VideoFragmentArgs.fromBundle(it).lessonNumber }
+        lesson_number.text = (position!! + 1).toString()
+    }
+
+}
