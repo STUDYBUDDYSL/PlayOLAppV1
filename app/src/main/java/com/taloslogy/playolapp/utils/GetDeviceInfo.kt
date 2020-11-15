2dcd15db626c3126921ceda10be61e1b5ac49cb0
rangana <rangana@taloslogy.com>
2020-11-15 17:48:14 +0530

    Add object method to get running device name

---
+++ app/src/main/java/com/taloslogy/playolapp/utils/GetDeviceInfo.kt
@@ -0,0 +1,28 @@
+package com.taloslogy.playolapp.utils
+
+import android.os.Build
+
+object GetDeviceInfo {
+
+    val deviceName: String? get() {
+        val manufacturer = Build.MANUFACTURER
+        val model = Build.MODEL
+        return if (model.toLowerCase().startsWith(manufacturer.toLowerCase())) {
+            capitalize(model)
+        } else {
+            capitalize(manufacturer).toString() + " " + model
+        }
+    }
+
+    private fun capitalize(s: String?): String? {
+        if (s == null || s.isEmpty()) {
+            return ""
+        }
+        val first = s[0]
+        return if (Character.isUpperCase(first)) {
+            s
+        } else {
+            Character.toUpperCase(first).toString() + s.substring(1)
+        }
+    }
+}
