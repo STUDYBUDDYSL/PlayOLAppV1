324aca627f5f83058bb4ee20ff7ba2b4e4ff2bf2
rangana <rangana@taloslogy.com>
2020-11-18 01:45:31 +0530

    Change proto file, add byte and hex magic classes using proto generated files

---
+++ app/src/main/java/com/taloslogy/playolapp/utils/ByteMagic.kt
@@ -0,0 +1,49 @@
+package com.taloslogy.playolapp.utils
+
+import com.google.protobuf.ByteString
+import com.taloslogy.playolapp.PlayOLProto
+
+class ByteMagic(var str: String) {
+
+    init {
+        val baseResponse = PlayOLProto.BaseResponse.newBuilder()
+            .setErrCode(0)
+            .build()
+        val initRequest = PlayOLProto.InitRequest.newBuilder()
+            .setBaseResponse(baseResponse)
+            .setChallenge(ByteString.copyFrom(GetDeviceInfo.deviceName, Charsets.UTF_8))
+            .build()
+
+        val au = PlayOLProto.InitRequest.parseFrom(initRequest.toByteArray()).challenge
+
+        val byteArray = getPass(str.chunked(3), devBytes = au.toByteArray())
+        val authResponse = PlayOLProto.AuthResponse.newBuilder()
+            .setInitRequest(initRequest)
+            .setAesSessionKey(ByteString.copyFrom(str, Charsets.UTF_8))
+            .build()
+        authResponse.toByteArray()
+
+        val p1 = PlayOLProto.AuthResponse.parseFrom(byteArray).aesSessionKey
+        str = p1.toStringUtf8()
+    }
+
+    private fun <T> concatenate(vararg lists: List<T>): List<T> {
+        return listOf(*lists).flatten()
+    }
+
+    private fun getPass(list: List<String>, devBytes: ByteArray): ByteArray {
+        val strs = getBytesAsString(list)
+        val devAsBytes = devBytes.map { it.toString(8) }
+        val final = concatenate(strs.subList(0,18), devAsBytes, strs.subList(18,strs.size))
+        return final.map {it.toByte(8)}.toByteArray()
+    }
+
+    private fun getBytesAsString(list: List<String>): ArrayList<String> {
+        val adder = SafetyNet.getAdder
+        val dedArr = arrayListOf<String>()
+        for(i in list.indices){
+            dedArr.add((list[i].toInt() - adder[i]).toString().padStart(3, '0'))
+        }
+        return dedArr
+    }
+}
---
+++ app/src/main/java/com/taloslogy/playolapp/utils/HexMagic.kt
@@ -0,0 +1,54 @@
+package com.taloslogy.playolapp.utils
+
+import com.google.protobuf.ByteString
+import com.taloslogy.playolapp.PlayOLProto
+
+class HexMagic(private val str: String) {
+
+    var keyPart = ""
+    var ivPart = ""
+
+    init {
+        val baseResponse = PlayOLProto.BaseResponse.newBuilder()
+            .setErrCode(0)
+            .build()
+        val initRequest = PlayOLProto.InitRequest.newBuilder()
+            .setBaseResponse(baseResponse)
+            .setChallenge(ByteString.copyFrom(GetDeviceInfo.deviceName, Charsets.UTF_8))
+            .build()
+        val au = PlayOLProto.InitRequest.parseFrom(initRequest.toByteArray()).challenge
+        val authResponse1 = PlayOLProto.AuthResponse.newBuilder()
+            .setInitRequest(initRequest)
+            .setAesSessionKey(ByteString.copyFrom(str, Charsets.UTF_8))
+            .build()
+        authResponse1.toByteArray()
+
+        val byteArray1 = str16ToByteArray(str.substring(0,58))
+        val p1 = PlayOLProto.BaseResponse.parseFrom(byteArray1).errmsg
+        val byteArray2 = str16ToByteArray(str.substring(58))
+        val p2 = PlayOLProto.BaseResponse.parseFrom(byteArray2).errmsg
+
+        if(au.toStringUtf8() == StringUtils.getDeviceName){
+            keyPart = getStringFromBytes(p1)
+            ivPart = getStringFromBytes(p2)
+        }
+    }
+
+    private fun str16ToByteArray(str: String): ByteArray {
+        val arr = arrayListOf<Int>()
+        for (x in str.indices step 2){
+            val s = str.substring(x, x+2)
+            val s10 = s.toInt(16)
+            arr.add(s10)
+        }
+        return arr.foldIndexed(ByteArray(arr.size)) { i, a, v -> a.apply { set(i, v.toByte()) } }
+    }
+
+    private fun getStringFromBytes(bytes: String?): String {
+        var str = ""
+        bytes?.forEach {
+            str += it.toString()
+        }
+        return str
+    }
+}
