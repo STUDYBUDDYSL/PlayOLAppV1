5c46d29e45594b03e85e8c64f64b38a48c130e4b
rangana <rangana@taloslogy.com>
2020-11-17 12:36:10 +0530

    Exit app if wrong device name or rooted

--- app/src/main/java/com/taloslogy/playolapp/utils/SafetyNet.kt
+++ app/src/main/java/com/taloslogy/playolapp/utils/SafetyNet.kt
@@ -1,21 +1,22 @@
 package com.taloslogy.playolapp.utils
 
-import android.content.Context
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
 
 object SafetyNet {
 
-    fun runSafetyNet(context: Context) {
+    fun runSafetyNet(context: AppCompatActivity) {
         val phoneModel = GetDeviceInfo.deviceName
         val isRooted = RootCheck.isDeviceRooted
 
-        if(phoneModel.equals(StringUtils.getDeviceName) || isRooted){
+        if(!phoneModel.equals(StringUtils.getDeviceName) || isRooted){
             val uri = Uri.parse(StringUtils.getExternalStorageTreePath)
             var docFile = DocumentFile.fromTreeUri(context,uri)
             docFile = docFile?.findFile(StringUtils.getCourseFolderName)
             docFile = docFile?.findFile(StringUtils.getKeyFileName)
             docFile?.delete()
+            context.finishAndRemoveTask()
         }
     }
 
