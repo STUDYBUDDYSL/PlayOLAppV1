4f92a264dac666cb2c9adf86ec30e057f56900b1
Rangana <rangana@taloslogy.com>
2021-06-09 15:11:35 +0530

    dynamically create stage buttons

--- app/src/main/java/com/taloslogy/playolapp/views/EnglishFragment.kt
+++ app/src/main/java/com/taloslogy/playolapp/views/EnglishFragment.kt
@@ -1,22 +1,21 @@
 package com.taloslogy.playolapp.views
 
 import android.os.Bundle
-import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.taloslogy.playolapp.R
 import com.taloslogy.playolapp.utils.FileUtils
 import com.taloslogy.playolapp.utils.StringUtils
-import kotlinx.android.synthetic.main.fragment_grade.*
-import kotlinx.android.synthetic.main.nice_button1.view.*
+import kotlinx.android.synthetic.main.fragment_english.*
+import kotlinx.android.synthetic.main.stage_btn.view.*
 import org.json.JSONObject
 import java.io.File
 import kotlin.concurrent.thread
-import kotlin.math.ceil
 
 /** @author Rangana Perera. @copyrights: Taloslogy PVT Ltd. */
 class EnglishFragment : Fragment() {
@@ -35,72 +34,40 @@ class EnglishFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val files = fUtils.getFilesFromPath("${StringUtils.getCoursePath}/English")
-        files.forEach {
-            Log.d("TEST_LOG", it.name)
-        }
+        thread { generateTable(files) }
 
     }
 
-    private fun generateTable(files: List<File>, grade: String?) {
+    private fun generateTable(files: List<File>) {
 
         val json = fUtils.readFileText(StringUtils.getJsonFileName, requireActivity())
         val jsonObject = JSONObject(json)
 
-        if (files.isNotEmpty()) {
-            // Create layout params for the table rows and children
-            val lParams = TableLayout.LayoutParams(
-                TableLayout.LayoutParams.MATCH_PARENT,
-                TableLayout.LayoutParams.WRAP_CONTENT,
-                3f
-            )
-            lParams.setMargins(0,10,0,10)
+        val width = resources.displayMetrics.widthPixels
 
-            val cParams = TableRow.LayoutParams(
-                0,
-                TableRow.LayoutParams.WRAP_CONTENT,
-                1f
-            )
+        val lParams = LinearLayout.LayoutParams(
+            getPxFromDp((width*0.6).toFloat()),
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        lParams.setMargins(0,10,0,10)
+        lParams.gravity = Gravity.END
 
-            // Set row count as per item count in files
-            val rowCount = ceil(files.size.div(3.0)).toInt()
-
-            for (x in 0 until rowCount) {
-                // Create a new row with layout params
-                val row = TableRow(activity)
-                row.layoutParams = lParams
-
-                // Assign children to the created row
-                var y = 0
-                while (y < 3) {
-                    val i = (x * 3) + y
-                    if (files.size > i && jsonObject.has(files[i].name)) {
-                        // Create the fancy button
-                        val nb = LayoutInflater.from(activity).inflate(R.layout.nice_button1, null)
-
-                        nb.btn.text = jsonObject.getJSONObject(files[i].name).getString("name")
-                        nb.btn.maxLines = jsonObject.getJSONObject(files[i].name).getInt("lines")
-                        nb.layoutParams = cParams
-
-                        nb.btn.setOnClickListener {
-                            val action =
-                                GradeFragmentDirections.selectSubject(
-                                    "$grade/${files[i].name}"
-                                )
-                            Navigation.findNavController(it).navigate(action)
-                        }
-
-                        row.addView(nb)
-                    }
-                    else {
-                        val extra = TextView(activity)
-                        extra.layoutParams = cParams
-                        row.addView(extra)
-                    }
-                    y++
-                }
-                // Add the completed row to table
-                requireActivity().runOnUiThread { table_view?.addView(row, lParams) }
+        for (x in files) {
+            val btnS = LayoutInflater.from(activity).inflate(R.layout.stage_btn, null)
+            btnS.stage_btn.text = x.name
+            btnS.layoutParams = lParams
+            // Add the completed row to table
+            requireActivity().runOnUiThread {
+                stages.addView(btnS)
             }
         }
+    }
+
+    private fun getPxFromDp(dp: Float): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            resources.displayMetrics
+        ).toInt()
     }
 }
